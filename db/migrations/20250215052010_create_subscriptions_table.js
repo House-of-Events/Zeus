93521dd7156af950677b421bcf5199c22edb1ef9
Manan <157731770+mananshah78424@users.noreply.github.com>
2025-02-15 19:27:48 -0800

    Creating subscriptions endpoint (#5)
    Co-authored-by: Manan Shah <manan@Manans-MacBook-Pro.local>

--- db/migrations/20250215052010_create_subscriptions_table.js
+++ db/migrations/20250215052010_create_subscriptions_table.js
@@ -2,27 +2,29 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function up(knex){
-    await knex.schema.createTable("subscriptions", function (table) {
-        table.string("id").primary();
-        table.string("user_id").notNullable().references("id").inTable("accounts");
-        table.string("channel_id").notNullable().references("id").inTable("channels");
-        table.string("channel_name").notNullable();
-        table.dateTime("subscribed_at").defaultTo(knex.fn.now());
-        table.boolean("is_active").defaultTo(true);
-        table.timestamps(true, true);
+export async function up(knex) {
+  await knex.schema.createTable("subscriptions", function (table) {
+    table.string("id").primary();
+    table.string("user_id").notNullable().references("id").inTable("accounts");
+    table
+      .string("channel_id")
+      .notNullable()
+      .references("id")
+      .inTable("channels");
+    table.string("channel_name").notNullable();
+    table.dateTime("subscribed_at").defaultTo(knex.fn.now());
+    table.boolean("is_active").defaultTo(true);
+    table.timestamps(true, true);
 
-        //contraints
-        table.unique(["user_id", "channel_id"]);
+    //contraints
+    table.unique(["user_id", "channel_id"]);
 
-        //Indexes
-        table.index("user_id");
-        table.index("channel_id");
-        table.index("is_active");
-    });
-
-};
-export async function down(knex){
-    await knex.schema.dropTable("subscriptions");  
-};
-
+    //Indexes
+    table.index("user_id");
+    table.index("channel_id");
+    table.index("is_active");
+  });
+}
+export async function down(knex) {
+  await knex.schema.dropTable("subscriptions");
+}
